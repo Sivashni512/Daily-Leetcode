66ac821f8fafafaafc7bcf10e773f29f662b90d4
Sivashni512 <sivashni512@gmail.com>
2026-07-11 19:05:29 +0530

    Add files via upload

---
+++ Codechef.java
@@ -0,0 +1,15 @@
+//Print the number 108 using arithmetic operations on the numbers 9 and 12.
+   
+
+
+
+class Codechef{
+    public static void main(String[] args) {
+        // Output only the result of the calculation
+        System.out.println(9 * 12);
+    }
+}
+
+
+
+
